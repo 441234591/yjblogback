d3dbb7bd698690bf719bcbe44b23d8a50275b893
yij <yij123456>
2021-05-17 15:52:38 +0800

    https文件

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var https = require('https');
+var http = require('http');
+var fs =require('fs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,8 +22,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//https读取密钥
+const httpsOption = {
+  key : fs.readFileSync("scs1617625734000_www.yijieback.xyz_server.key"),
+  cert: fs.readFileSync("scs1617625734000_www.yijieback.xyz_server.crt")
+}
+var httpsServer=https.createServer(httpsOption, app);
+
+var httpServer=http.createServer(app);
+
+app.use('/',(req,res,next)=>{
+  //console.log(req);
+  console.log("router 请求");
+  next();
+})
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+httpsServer.listen(443);
+httpServer.listen(80);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
