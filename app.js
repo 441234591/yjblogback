3eb6bfa759e237cb6952602ec397a8ce8f2dcfc3
yij <yij123456>
2021-05-18 15:11:08 +0800

    https修改

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use('/',(req,res,next)=>{
 app.use('/', indexRouter);
 app.use('/api/user', usersRouter);
 
-httpsServer.listen(443);
+//httpsServer.listen(443);
 //httpServer.listen(3000);
 
 // catch 404 and forward to error handler
