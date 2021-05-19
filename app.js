80d1f8c12747839fa9e86410448e01f992ba2c77
yij <yij123456>
2021-05-19 11:30:32 +0800

    登录接口

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var https = require('https');
 var http = require('http');
 var fs =require('fs');
 const cors = require('cors');
+const expressJWT = require('express-jwt');
+const {PRIVATE_KEY} = require('./utils/constant');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,6 +25,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
+app.use(expressJWT({
+  secret: PRIVATE_KEY,
+  algorithms:['HS256'],
+  credentialsRequired:true   
+}).unless({
+  path: ['/api/user/register','/api/user/login','/api/user/upload','/api/article/allList','/api/article/detail','/api/comment/list']  //白名单,除了这里写的地址，其他的URL都需要验证
+}));
 //https读取密钥
 const httpsOption = {
   key : fs.readFileSync("scs1617625734000_www.yijieback.xyz_server.key"),
@@ -51,13 +60,18 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  if (err.name === 'UnauthorizedError') {   
+    //  这个需要根据自己的业务逻辑来处理
+    res.status(401).send({code:-1,msg:'token验证失败'});
+  }else{
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  }
 });
 
 module.exports = app;
---
+++ utils/constant.js
@@ -0,0 +1,5 @@
+module.exports = {
+    PWD_SALT:'yj_node',
+    PRIVATE_KEY:'yj_blog',
+    EXPIRESD:60*60*24
+}
