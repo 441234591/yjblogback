e03da0b389aed2232a94baa48705808666e73565
yij <441234591@qq.com>
2022-02-09 17:16:12 +0800

    修改文件地址

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       host : '121.5.236.190',
       ref  : 'origin/master',
       repo : 'git@github.com:441234591/yjblogback.git',
-      path : '/www/wwwroot',
+      path : '/www/wwwroot/blog_serve',
       ssh_options:"StrictHostKeyChecking=no",
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
